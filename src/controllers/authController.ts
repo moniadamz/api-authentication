83908d6163258b31b04242f0ca7296119ecbd287
monica <monica_adamski@hotmail.com>
2020-04-25 22:37:33 -0300

    feat: adding jwt authentication

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,7 +1,19 @@
-export class AuthController {
-    constructor() {}
+import * as jwt from "jsonwebtoken"
+import { Request, Response } from "express"
 
-    async authenticate(req: Request, res: Response) {
-        
-      }
+export class AuthController {
+  constructor() {}
+
+  async authenticate(req: Request, res: Response) {
+    const { user, password } = req.body
+
+    if (user !== process.env.USER || password !== process.env.PASSWORD) {
+      res.send({ message: "User or password invalid." })
+    }
+    res.send({
+      token: jwt.sign({ user, password }, process.env.SECRET, {
+        expiresIn: 86400
+      })
+    })
+  }
 }
